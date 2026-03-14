9f0baf458cd6b9facb758b06a319d2e0fb3d6fd8
Cazador457 <166962932+Misalmon1341@users.noreply.github.com>
2026-03-13 18:58:09 -0600

    Creando UIManager

--- Frog_Game/Assets/Code/UI/UIWindow.cs
+++ Frog_Game/Assets/Code/UI/UIWindow.cs
@@ -1,26 +1,44 @@
+using NaughtyAttributes;
+using NUnit.Framework.Constraints;
 using UnityEngine;
 
 public class UIWindow : MonoBehaviour
 {
-    [Header("UI Window")]
-    [SerializeField] private string windowId;
+    [Header("UI Window")] [SerializeField] private string windowId;
+    
+    [Header("UI References")]
+    [SerializeField] private Canvas canvas;
+    [SerializeField] private CanvasGroup canvasGroup;
+
+    [SerializeField] private bool hideOnStart = true;
+    
+    public string WindowId => windowId;
     private void Awake()
     {
-        
+
     }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+     Initialize();
     }
-
+    
+    
+    public void Initialize()
+    { 
+        canvas.gameObject.SetActive(!hideOnStart);
+    }
+ 
+    [Button]
     public virtual void Show()
     {
-        
+        canvas.gameObject.SetActive(true);
     }
-
+    
+    [Button]
     public virtual void Hide()
     {
-        
+        canvas.gameObject.SetActive(false);
     }
 }
--- Frog_Game/Assets/Code/UI/UiManager.cs
+++ Frog_Game/Assets/Code/UI/UiManager.cs
@@ -1,14 +1,57 @@
+using System.Collections.Generic;
+using NaughtyAttributes;
 using UnityEngine;
 
 public class UiManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private List<UIWindow> windows = new List<UIWindow>(); 
     void Start()
     {
-        
+        Initialize();
     }
 
-    // Update is called once per frame
+    private void Initialize()
+    {
+    }
+    
+    
+
+    [Button]
+    public void ShowPopup()
+    {
+        ShowWindow("popupui");
+    }
+
+    private void FoundUIScene()
+    {
+        windows.Clear();
+        var uiWindows = gameObject.GetComponentsInChildren<UIWindow>(true);
+        foreach (var uiWindow in uiWindows)
+        {
+            if (!windows.Contains(uiWindow))
+            {
+                windows.Add(uiWindow);
+            }
+        }
+    }
+
+    public void ShowWindow(string windowId)
+    {
+        //var uiWindow = windows.Find(x => x.name == windowId);
+
+        foreach (UIWindow window in windows)
+        {
+            if (window.WindowId == windowId)
+            {
+                window.Show();
+                return;
+            }
+        }
+        
+        //if (windowToShow != null))
+    }
+
+ 
     void Update()
     {
         
