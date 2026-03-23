f86c497351df4c4d73de2155eedf1f8ec7195c48
Misael Bautista <166962932+Misalmon1341@users.noreply.github.com>
2026-03-22 23:11:22 -0600

    Storewindow

---
+++ Frog_Game/Assets/Code/UI/EquipUI.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class EquipUI : UIWindow
+{
+    public override void Show()
+    {
+        base.Show();
+    }
+
+    public override void Hide()
+    {
+        base.Hide();
+    }
+}
---
+++ Frog_Game/Assets/Code/UI/PurchaseUI.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PurchaseUI : UIWindow
+{
+    public override void Show()
+    {
+        base.Show();
+    }
+    
+    public override void Hide()
+    {
+        base.Hide();
+    }
+}
