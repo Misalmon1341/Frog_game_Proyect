266c7ea3cef34a363159a420253108b406c2785a
Misael Bautista <misagamer559@gmail.com>
2026-04-14 19:13:32 -0600

    Arreglando carpetas

--- Frog_Game/Assets/Code/UI/UIWindow.cs
+++ Frog_Game/Assets/Code/UI/UIWindow.cs
@@ -30,8 +30,7 @@ public class UIWindow : MonoBehaviour
     {
 
     }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
      Initialize();
