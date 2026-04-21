564328ed30bd6b927a2e20f06ff9194b1f8f996e
Misalmon1341 <166962932+Misalmon1341@users.noreply.github.com>
2026-04-21 17:16:27 -0600

    Añadiendo grid

---
+++ Frog_Game/Assets/Code/GamePlay/GridElement.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+public class GridElement : MonoBehaviour
+{
+   public int columns;
+   public int rows;
+
+   private void Start()
+   {
+      for (int i = 0; i <= columns; i++)
+      {
+         
+      }
+   }
+}
